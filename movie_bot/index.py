3cf5bf8600dbc6a2797a743e838899d5b53ab979
Developer199239 <developer199239@gmail.com>
2019-11-05 21:31:54 +0600

    add microphone

--- movie_bot/index.py
+++ movie_bot/index.py
@@ -4,6 +4,8 @@ import dialogflow
 import requests
 import json
 import pusher
+import speech_recognition as sr
+import webbrowser as wb
 
 # initialize Pusher
 pusher_client = pusher.Pusher(
@@ -20,55 +22,88 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/get_movie_detail', methods=['POST'])
-# def get_movie_detail():
-#     data = request.get_json(silent=True)
-#     movie = data['queryResult']['parameters']['movie']
-#     api_key = os.getenv('OMDB_API_KEY')
-#
-#     movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
-#     movie_detail = json.loads(movie_detail)
-#     response = """
-#             Title : {0}
-#             Released: {1}
-#             Actors: {2}
-#             Plot: {3}
-#         """.format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Plot'])
-#
-#     reply = {
-#         "fulfillmentText": response,
-#     }
-#
-#     return jsonify(reply)
-#
-#
-# @app.route('/send_message', methods=['POST'])
-# def send_message():
-#     message = request.form['message']
-#     project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-#     fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
-#     response_text = {"message": fulfillment_text}
-#
-#     socketId = request.form['socketId']
-#     pusher_client.trigger('movie_bot', 'new_message',
-#                           {'human_message': message, 'bot_message': fulfillment_text},
-#                           socketId)
-#     return jsonify(response_text)
-#
-#
-# def detect_intent_texts(project_id, session_id, text, language_code):
-#     session_client = dialogflow.SessionsClient()
-#     session = session_client.session_path(project_id, session_id)
-#
-#     if text:
-#         text_input = dialogflow.types.TextInput(
-#             text=text, language_code=language_code)
-#         query_input = dialogflow.types.QueryInput(text=text_input)
-#         response = session_client.detect_intent(
-#             session=session, query_input=query_input)
-#
-#         return response.query_result.fulfillment_text
-#
+
+@app.route('/get_movie_detail', methods=['POST'])
+def get_movie_detail():
+    data = request.get_json(silent=True)
+    movie = data['queryResult']['parameters']['movie']
+    api_key = os.getenv('OMDB_API_KEY')
+
+    movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
+    movie_detail = json.loads(movie_detail)
+    response = """
+            Title : {0}
+            Released: {1}
+            Actors: {2}
+            Plot: {3}
+        """.format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Plot'])
+
+    reply = {
+        "fulfillmentText": response,
+    }
+
+    return jsonify(reply)
+
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+    response_text = {"message": fulfillment_text}
+
+    socketId = request.form['socketId']
+    pusher_client.trigger('movie_bot', 'new_message',
+                          {'human_message': message, 'bot_message': fulfillment_text},
+                          socketId)
+    return jsonify(response_text)
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        return response.query_result.fulfillment_text
+
+
+@app.route('/background_process_test')
+def background_process_test():
+    print("ok")
+    return "nothing"
+
+
+@app.route('/send_message1', methods=['POST'])
+def send_message1():
+    # message = request.form['message']
+    # return message
+    r1 = sr.Recognizer()
+    r2 = sr.Recognizer()
+    r3 = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("search youtube")
+        print('speak now')
+        audio = r3.listen(source)
+
+        print('speak now')
+
+        try:
+            get = r3.recognize_google(audio)
+            print(get)
+            return get
+        except sr.UnknownValueError:
+            print('error')
+            return 'error'
+        except sr.RequestError as e:
+            print('failed'.format(e))
+            return e
 
 
 # run Flask app
--- movie_bot/static/custom.js
+++ movie_bot/static/custom.js
@@ -1,3 +1,12 @@
+// Initialise Pusher
+const pusher = new Pusher('ab111db1de74ab5d6b29', {
+    cluster: 'ap2',
+    encrypted: true
+});
+
+// Subscribe to movie_bot channel
+const channel = pusher.subscribe('movie_bot');
+
 $(function() {
   $("#addClass").click(function(e) {
     e.preventDefault();
@@ -6,12 +15,131 @@ $(function() {
   $("#removeClass").click(function() {
     $('#qnimate').removeClass('popup-box-on');
   });
+
+  $("#speech_recog").click(function(e) {
+    e.preventDefault();
+        var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 5;
+recognition.start();
+
+recognition.onresult = function(event) {
+//    console.log('You said: ', event.results[0][0].transcript);
+    userSays(event.results[0][0].transcript)
+};
+
+  });
+
+
 });
+
+ function submit_message(message) {
+
+$.post( "/send_message", {
+            message: message,
+            socketId: pusher.connection.socket_id
+        }, handle_response);
+
+        function handle_response(data) {
+            // append the bot repsonse to the div
+            console.log(data.message)
+            $('.direct-chat-msg').append(`
+            <div class="direct-chat-text">
+                    ${data.message}
+                </div>
+`)
+            // remove the loading indicator
+            $( "#loading" ).remove();
+
+            $('.direct-chat-msg').append(`
+            <div class="margin_div">
+            </div>
+        `)
+
+
+//        const messages = document.getElementsByClassName(".popup-messages");
+//        messages.scrollTop = messages.scrollHeight;sName(".popup-messages");
+//        messages.scrollTop = messages.scrollHeight;sName(".popup-messages");
+//        messages.scrollTop = messages.scrollHeight;
+
+
+//showHeight( "popup-messages", $( document ).height() );
+//
+//showHeight( "direct-chat-messages", $( document ).height() );
+//
+//showHeight( "direct-chat-msg", $( document ).height() );
+showHeight( "direct-chat-msg", $( document ).height() );
+
+//var objDiv = document.getElementsByClassName("#popup-messages");
+//        $('.popup-messages').scroll(objDiv.scrollHeight);
+
+        }
+
+
+}
+
+function showHeight( element, height ) {
+  $('.popup-messages').scroll(height);
+}
 
 
 $('#target').on('submit', function(e){
         e.preventDefault();
         const input_message = $('#input_message').val()
-        // clear the text input
-        $('#input_message').val('jaliur')
+        // return if the user does not enter any text
+        if (!input_message) {
+            return
+        }
+
+        userSays(input_message);
+
+
     });
+
+
+function userSays(input_message){
+
+    //        user part
+        const userName = 'User'
+        const robotName = 'Robot'
+        const loadingMsg = 'Loading...'
+        const userImage = '//www.gravatar.com/avatar/2090713a50fcdae69b802e73815000a4/?default=&s=64'
+        const robotImage = 'https://robohash.org/69.162.124.234.png'
+        $('.direct-chat-msg').append(`
+            <div class="robot_direct-chat-info clearfix">
+                    <span class="robot_direct-chat-name pull-right">${userName}</span>
+                </div>
+                <img alt="message user image"
+                     src=${userImage}
+                     class="robot_direct-chat-img">
+
+                     <div class="robot_direct-chat-text">
+                    ${input_message}
+                </div>
+        `)
+
+        $('.direct-chat-msg ').append(`
+            <div class="margin_div">
+            </div>
+        `)
+
+
+        $('.direct-chat-msg ').append(`
+        <div class="direct-chat-info clearfix">
+                    <span class="direct-chat-name pull-left">${robotName}</span>
+              </div>
+                <img alt="message user image"
+                     src=${robotImage}
+                     class="direct-chat-img">
+
+                     <div class="direct-chat-text" id="loading">
+                    ${loadingMsg}
+                </div>
+        `)
+
+
+//        robot part
+$('#input_message').val('')
+         submit_message(input_message)
+}
